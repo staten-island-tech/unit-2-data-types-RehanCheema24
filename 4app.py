3844b5037eba1c061ebbf79ffac51acfd6d30a81
RehanCheema24 <rehanc4@nycstudents.net>
2024-09-17 10:13:59 -0400

    create 4app.py

---
+++ 4app.py
@@ -0,0 +1,28 @@
+""" x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+""" input("How are you")
+input("good") """
+""" day_of_week = input("Tuesday")
+if day_of_week == "Tuesday":
+    print("correct")
+else:
+    print("incorrect")
+day_of_week = Tuesday
+ """
+ 
+""" temp = 58
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold') """
+values = {2,4,6,8,10}
+if x = values:
+    print(even)
+else:
+print(odd)
+
