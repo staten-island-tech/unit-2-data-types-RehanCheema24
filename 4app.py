dcdeaa80bdaebb205991bcd9e60c20aa8fd59c8b
RehanCheema24 <rehanc4@nycstudents.net>
2024-09-18 10:14:01 -0400

    update4.apppy

--- 4app.py
+++ 4app.py
@@ -20,9 +20,40 @@ elif temp == 68:
     print('perfect')
 else:
     print('cold') """
-values = {2,4,6,8,10}
-if x = values:
-    print(even)
+""" values = [2,4,6,8,10]
+x = values
+if x == values:
+    print ("Even")
 else:
-print(odd)
+    print ("Odd") """
+""" x= 31
+quotient, remainder = divmod(x,2)
+if remainder == 1:
+    print ("Odd")
+else:
+    print("Even") """
+""" def bad(q):
+    q = ("0%")
+if service == bad:
+    print("0%") """
+def bad(q):
+    q = ("0%")
+service = bad
+if service == bad:
+    print("0%")
+def okay(m):
+    m = ("15%")
+service = okay
+if service == okay:
+    print ("15%")
+def good(v):
+    q = ("20%")
+service = good
+if service == good:
+    print("20%")
+
+
+
+
+
 
